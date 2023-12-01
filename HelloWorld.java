8dd7fc09f25243401850c44e67ebc4ea5e067cb8
noahlot <152526823+noahlot@users.noreply.github.com>
2023-12-01 15:41:20 -0500

    Create HelloWorld.java
    Adding a file of a basic Java program which prints "Hello, World!" as an example

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Filename: HelloWorld.java
+ * Description: An example program that prints Hello World
+ * @author Noah Barajas
+ * @date 2023/12/01
+ */
+ 
+ public class HelloWorld {
+   public static void main(String[] args) {
+      System.out.print("Hello, World!");
+   }
+ }
